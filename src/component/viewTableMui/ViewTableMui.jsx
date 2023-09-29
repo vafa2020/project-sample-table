f3dd9031271adcc2a401de11eb855f03d83df536
unknown <vafaee.mh@gmail.com>
2023-09-29 23:52:54 +0330

    nothing

--- src/component/viewTableMui/ViewTableMui.jsx
+++ src/component/viewTableMui/ViewTableMui.jsx
@@ -26,9 +26,7 @@ import { Loading } from "../Loading";
 
 const ViewTableMui = () => {
   const dispatch = useDispatch();
-  const { data, loading, error, customer } = useSelector(
-    (state) => state.Table
-  );
+  const { data, loading } = useSelector((state) => state.Table);
   const [createModalOpen, setCreateModalOpen] = useState(false);
   const [validationErrors, setValidationErrors] = useState({});
 
@@ -38,19 +36,13 @@ const ViewTableMui = () => {
 
   const handleCreateNewRow = (values) => {
     dispatch(addCustomer(values));
-
-    // tableData.push(values);
-    // setTableData([...tableData]);
   };
 
-  const handleSaveRowEdits = async ({ exitEditingMode, row, values }) => {
+  const handleSaveRowEdits = async ({ exitEditingMode, values }) => {
     console.log(values);
     if (!Object.keys(validationErrors).length) {
       dispatch(editCustomer(values));
-      //   tableData[row.index] = values;
-      //send/receive api updates here, then refetch or update local table data for re-render
-      //   setTableData([...tableData]);
-      exitEditingMode(); //required to exit editing mode and close modal
+      exitEditingMode();
     }
   };
 
@@ -105,14 +97,6 @@ const ViewTableMui = () => {
 
   const columns = useMemo(
     () => [
-      {
-        accessorKey: "id",
-        header: "ID",
-        enableColumnOrdering: false,
-        enableEditing: false, //disable editing on this column
-        enableSorting: false,
-        size: 80,
-      },
       {
         accessorKey: "number",
         header: "Number",
@@ -208,7 +192,7 @@ const ViewTableMui = () => {
             onClick={() => setCreateModalOpen(true)}
             variant="contained"
           >
-            Create New Account
+            Create New Customer
           </Button>
         )}
       />
@@ -222,7 +206,6 @@ const ViewTableMui = () => {
   );
 };
 
-//example of creating a mui dialog modal for creating new rows
 export const CreateNewAccountModal = ({ open, columns, onClose, onSubmit }) => {
   const [values, setValues] = useState(() =>
     columns.reduce((acc, column) => {
@@ -272,15 +255,5 @@ export const CreateNewAccountModal = ({ open, columns, onClose, onSubmit }) => {
     </Dialog>
   );
 };
-
-const validateRequired = (value) => !!value.length;
-const validateEmail = (email) =>
-  !!email.length &&
-  email
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
-const validateAge = (age) => age >= 18 && age <= 50;
 
 export default ViewTableMui;
